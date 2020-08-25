cd3684ac887e8eb521dcc1470f3fced9c96ab87b
John Bowser <john.bow7720@gmail.com>
2020-08-25 16:30:40 -0600

    replaced db variables

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const knex = require('knex')
 
-const { PORT, DATABASE_URL } = require('./config')
+const { PORT, DATABASE_URL, NODE_ENV } = require('./config')
 
 const db = knex({
   client: 'pg',
--- test/note-endpoint.spec.js
+++ test/note-endpoint.spec.js
@@ -14,7 +14,7 @@ describe('notes endpoint', () => {
   before(() => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     })
     app.set('db', db)
   })
